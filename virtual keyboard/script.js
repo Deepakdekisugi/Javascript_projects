973faf46687be6474c5f952f91c68eeb122af2f0
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-07-16 22:41:28 +0530

    updated script.js

--- virtual keyboard/script.js
+++ virtual keyboard/script.js
@@ -16,14 +16,41 @@ const Keyboard = {
     },
 
     init() {
+        //create main element
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
 
+        //setup main elements
+        this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+
+        //Add to Dom
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
     },
 
     _createKeys() {
+        cosnt fragment = document.createDocumentFragment();
+        const keyLayout
+    },
+
+    _triggerEvent(handlerName) {
+        console.log("Event Triggered! Event Name: " + handlerName);
+    },
+
+    _toggleCapsLock() {
+        console.log("Caps Lock Toggled!");
+    },
+
+    open(initialValue, oninput, onclose) {
 
     },
 
-    _triggerEvent() {
-        
+    close() {
+
     }
 };
+
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init();
+});
