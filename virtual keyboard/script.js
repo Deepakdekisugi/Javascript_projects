3c9537e06a3c6cea05418314485100f66e8cad63
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-07-16 13:47:27 +0530

    updated script.js

---
+++ virtual keyboard/script.js
@@ -0,0 +1,29 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false
+    },
+
+    init() {
+
+    },
+
+    _createKeys() {
+
+    },
+
+    _triggerEvent() {
+        
+    }
+};
