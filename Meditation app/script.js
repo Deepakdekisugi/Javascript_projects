5f0e12beae0726d19d4382f9b3d38fae8668a615
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-08-01 23:57:17 +0530

    updated script.js

--- Meditation app/script.js
+++ Meditation app/script.js
@@ -9,6 +9,7 @@ const app = () => {
 
   //Time Display
   const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
 
   //Get the outline length of the circle
   const outlineLength = outline.getTotalLength();
@@ -18,6 +19,56 @@ const app = () => {
 
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
+
+  //play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //select sound
+  timeSelect.forEach((option) => {
+    option.addEventListener("click", function () {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
+  });
+
+  //Function to stop and play
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
+
+  //Animate the circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    //Bar animation
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    // /Text animation
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
+  };
 };
 
 app();
