2be11026beba2a1f67b9ccf70043c9f32b4822ca
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-08-05 15:49:30 +0530

    declared all variables

---
+++ Shopping cart/script.js
@@ -0,0 +1,28 @@
+// Variable
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+//Cart
+let cart = [];
+
+// Getting the products
+class Products {}
+
+// Display products
+class UI {}
+
+// Local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
