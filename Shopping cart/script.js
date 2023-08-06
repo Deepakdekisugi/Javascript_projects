263af4ea6b9c7d2f0bfed52aa734a29d109ba5b4
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-08-06 23:16:56 +0530

    completed get products section

--- Shopping cart/script.js
+++ Shopping cart/script.js
@@ -19,7 +19,15 @@ class Products {
     try {
       let result = await fetch("products.json");
       let data = await result.json();
-      return data;
+
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {
       console.log(error);
     }
@@ -37,5 +45,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   // Get all products
-  products.getProducts().then((data) => console.log(data));
+  products.getProducts().then((products) => console.log(products));
 });
