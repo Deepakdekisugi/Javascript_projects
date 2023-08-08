a2e2df90f05f8711cc5155830658b8608979911b
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-08-08 23:53:20 +0530

    added cart item details

--- Shopping cart/script.js
+++ Shopping cart/script.js
@@ -84,6 +84,7 @@ class UI {
         // Set cart values
         this.setCartValues(cart);
         // Display cart items
+        this.addCartItem(cartItem);
         // Show the cart
       });
     });
@@ -97,7 +98,23 @@ class UI {
     });
     cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
     cartItems.innerText = itemsTotal;
-    console.log(cartTotal, cartItems);
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `<img src=${item.image} alt="product" />
+    <div>
+      <h4>${item.title}</h4>
+      <h5>$${item.price}</h5>
+      <span class="remove-item" data-id=${item.id}>Remove</span>
+    </div>
+    <div>
+      <i class="fas fa-chevron-up" data-id=${item.id}></i>
+      <p class="item-amount">${item.amount}</p>
+      <i class="fas fa-chevron-down" data-id=${item.id}></i>
+    </div>`;
+    cartContent.appendChild(div);
+    console.log(cartContent);
   }
 }
 
