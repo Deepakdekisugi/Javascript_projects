84702e7973b6cfde7e1a697def02c75c2d561d5e
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-08-11 00:00:24 +0530

    added +1 item

--- Shopping cart/script.js
+++ Shopping cart/script.js
@@ -149,6 +149,8 @@ class UI {
       } else if (event.target.classList.contains("fa-chevron-up")) {
         let addAmount = event.target;
         let id = addAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount + 1;
       }
     });
   }
