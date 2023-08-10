726aa6448e8969461391b0212ac0c1ebafbc97fb
Deepak <55588916+Deepakdekisugi@users.noreply.github.com>
2023-08-11 01:59:13 +0530

    added increase/ decrease amount in cart

--- Shopping cart/script.js
+++ Shopping cart/script.js
@@ -151,6 +151,22 @@ class UI {
         let id = addAmount.dataset.id;
         let tempItem = cart.find((item) => item.id === id);
         tempItem.amount = tempItem.amount + 1;
+        Storage.saveCart(cart);
+        this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      } else if (event.target.classList.contains("fa-chevron-down")) {
+        let lowerAmount = event.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = cart.find((item) => item.id === id);
+        tempItem.amount = tempItem.amount - 1;
+        if (tempItem.amount > 0) {
+          Storage.saveCart(cart);
+          this.setCartValues(cart);
+          lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        } else {
+          cartContent.removeChild(lowerAmount.parentElement.parentElement);
+          this.removeItem(id);
+        }
       }
     });
   }
