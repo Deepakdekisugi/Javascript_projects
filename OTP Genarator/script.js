e40d7f91be80eb1bfecc9a6e596bc55f0bebdeda
Deepak <dk376907@gmail.com>
2023-11-10 21:50:14 +0530

    added mobile number fetch prompt

--- OTP Genarator/script.js
+++ OTP Genarator/script.js
@@ -15,12 +15,56 @@ function generateOTPs(){
         "" +
         Math.floor(Math.random() * 10)
     );
-
-    expire.innerText = 10;
+    
+    inputs[0].focus();
+    expire.innerText = 30;
     const expireInterval = setInterval(function (){
         expire.innerText--;
         if(expire.innerText == 0){
             clearInterval(expireInterval)
         };
-    }, 1000)
+    }, 1000);
 }
+
+function clearOTPs(){}
+
+inputs.forEach((input, index) => {
+    input.addEventListener("keyup", function(e){
+        const currentInput = input,
+            nextInput = input.nextElementSibling,
+            prevInput = input.previousElementSibling;
+        
+        // console.log(currentInput, nextInput, prevInput);
+
+        if(nextInput && nextInput.hasAttribute("disabled") &&
+        currentInput.value!==""){
+            nextInput.removeAttribute("disabled", true);
+            nextInput.focus();
+        }
+
+        if(e.key === "Backspace"){
+            inputs.forEach((input, index1) => {
+                if(index <= index1 && prevInput){
+                input.setAttribute("disabled", true);
+                prevInput.focus();
+                prevInput.value = "";
+                }
+            });
+        }
+
+        if(!inputs[3].disabled && inputs[3].value !== ""){
+            inputs[3].blur();
+            button.classList.add("active");
+            return;
+        }
+        button.classList.remove("active");
+    });
+});
+
+
+window.addEventListener("load", () =>{
+    let x = prompt("Please enter your mobile number to verify ");
+    if(x){
+        mobile.innerText = x;
+    }
+})
