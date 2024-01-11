7754b2618a773970d68277f1786db20c8d1800e0
Deepak <dk376907@gmail.com>
2024-01-11 16:09:49 +0530

    created init function

--- Complex Sort/script.js
+++ Complex Sort/script.js
@@ -9,3 +9,20 @@ const spacing = (myCanvas.width - margin * 2) / n;
 const ctx = myCanvas.getContext("2d");
 
 const maxColumnHeight = 200;
+
+init();
+
+
+function init() {
+    for (let i = 0; i < n; i++) {
+      array[i] = Math.random();
+    }
+    moves = [];
+    for (let i = 0; i < array.length; i++) {
+      const x = i * spacing + spacing / 2 + margin;
+      const y = myCanvas.height - margin - i * 3;
+      const width = spacing - 4;
+      const height = maxColumnHeight * array[i];
+      cols[i] = new Column(x, y, width, height);
+    }
+  }
