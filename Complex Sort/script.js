a86a5d736e45f66003d1ec25a5c8c8f810d39e7a
Deepak <dk376907@gmail.com>
2024-01-11 16:16:32 +0530

    Update script.js

--- Complex Sort/script.js
+++ Complex Sort/script.js
@@ -26,3 +26,45 @@ function init() {
       cols[i] = new Column(x, y, width, height);
     }
   }
+
+  function bubbleSort(array) {
+    const moves = [];
+    do {
+      var swapped = false;
+      for (let i = 1; i < array.length; i++) {
+        if (array[i - 1] > array[i]) {
+          swapped = true;
+          [array[i - 1], array[i]] = [array[i], array[i - 1]];
+          moves.push({ indices: [i - 1, i], swap: true });
+        } else {
+          moves.push({ indices: [i - 1, i], swap: false });
+        }
+      }
+    } while (swapped);
+    return moves;
+  }
+
+  function animate() {
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    let changed = false;
+    for (let i = 0; i < cols.length; i++) {
+      changed = cols[i].draw(ctx) || changed;
+    }
+  
+    if (!changed && moves.length > 0) {
+      const move = moves.shift();
+      const [i, j] = move.indices;
+      const waveformType = move.swap ? "square" : "sine";
+      playNote(cols[i].height + cols[j].height, waveformType);
+      if (move.swap) {
+        cols[i].moveTo(cols[j]);
+        cols[j].moveTo(cols[i], -1);
+        [cols[i], cols[j]] = [cols[j], cols[i]];
+      } else {
+        cols[i].jump();
+        cols[j].jump();
+      }
+    }
+  
+    requestAnimationFrame(animate);
+  }
